95959fe8005aa01963b86a843fc58f6179664877
Andrei <dandrei.web@gmail.com>
2024-10-23 22:58:50 +0200

    FE Update: Login
    validateForm, loading & error messages

--- frontend/src/components/Auth/Login.tsx
+++ frontend/src/components/Auth/Login.tsx
@@ -4,21 +4,61 @@ import axios from "axios";
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const apiUrl = import.meta.env.VITE_API_URL;
+
+  const validateForm = () => {
+    setError(null);
+
+    if (!email.trim()) {
+      setError("Email is required");
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setError("Please enter a valid email address");
+      return false;
+    }
+
+    // Password validation
+    if (!password.trim()) {
+      setError("Password is required");
+      return false;
+    }
+    if (password.length < 8) {
+      setError("Password must be at least 8 characters long");
+      return false;
+    }
+
+    return true;
+  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    if (!validateForm()) return;
+
     try {
+      setIsLoading(true);
+
       const response = await axios.post(apiUrl + "/auth/login", {
         email,
         password,
       });
 
       console.log(response);
-    } catch (error) {
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error) && error.response?.status === 401) {
+        setError("Password or email is incorrect");
+      } else {
+        setError("An unexpected error occurred. Please try again later.");
+      }
       console.log(error);
+      
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -26,8 +66,16 @@ export const Login = () => {
     <div>
       <h1 className="text-2xl font-bold mb-4">Login</h1>
       <form className="flex flex-col space-y-4 w-64" onSubmit={handleSubmit}>
+        {error && (
+          <div
+            className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4"
+            role="alert"
+          >
+            <p>{error}</p>
+          </div>
+        )}
         <input
-          type="email"
+          type="text"
           placeholder="Email"
           className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           value={email}
@@ -44,7 +92,7 @@ export const Login = () => {
           type="submit"
           className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition duration-300"
         >
-          Login
+          {isLoading ? "Loading..." : "Login"}
         </button>
       </form>
     </div>
